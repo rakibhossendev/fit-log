ee4be442914db154dc65264594f2d7cf0c1d8f94
rakibhossendev <rh01828315879@gmail.com>
2026-09-26 21:24:14 +0600

    Add localstorage and fire logo

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,7 +7,6 @@ export default async function PlanPage(){
     const response = await fetch('https://api.abcz.workers.dev/api/fitlog',{cache:"force-cache"});
     const planData: PlanDataType[] = await response.json();
 
-    // console.log(data);
 
     
     return (
